bf6df54eb3dc99a6c7bfdc7c5c1398672755052f
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-14 16:34:40 +0200

    add ternary operator on class nav( header.blade)

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,10 +9,12 @@
             <div>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link {{ request()->routeIs('admin.home') ? 'active' : '' }}" aria-current="page"
+                            href="{{ route('admin.home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('comics.index') }}">Comic</a>
+                        <a class="nav-link {{ request()->routeIs('comics.index') ? 'active' : '' }}"
+                            href="{{ route('comics.index') }}">Comic</a>
                     </li>
                 </ul>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('admin.home');
+
 Route::resource('comics', 'ComicController');
