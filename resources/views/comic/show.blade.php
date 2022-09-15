6db0af0a17e715c19cd45900ece83085ee3efd76
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-15 14:52:47 +0200

    add btn (edit , delete) on show.blade

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -17,8 +17,22 @@
             <P>TYPE : {{ $comic->type }}</P>
             <P>SERIES : {{ $comic->series }}</P>
         </div>
-        <div class="card-footer text-muted">
+        <div class="card-footer d-flex justify-content-center">
+            <div class="mx-3">
+                <a class="btn btn-success" href="{{ route('comics.edit', $comic->slug) }}">
+                    Edit
+                </a>
+            </div>
+            <div class="mx-3">
+                <form action="{{ route('comics.destroy', $comic->slug) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
 
+                    <button class="btn btn-danger" type="submit">
+                        Delete
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
