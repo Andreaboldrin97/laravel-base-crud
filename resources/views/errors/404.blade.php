61cf7813f3465830b2e3be2ce94940ffa1de734d
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-16 17:59:36 +0200

    create 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('name', '404')
+
+
+
+@section('main-content')
+    <div class="w-100">
+        <h1 class="text-center">404 - NOT FOUND</h1>
+        <div class="d-flex justify-content-center">
+            <img src="https://hookagency.com/wp-content/uploads/2019/08/404-message.jpg" alt="404-img">
+        </div>
+    </div>
+@endsection
